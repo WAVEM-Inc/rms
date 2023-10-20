2061ab88955ff43a327c6126171ba8d581b4733b
reidlo5135 <201840103@email.daelim.ac.kr>
2023-10-20 09:50:46 +0900

    refactor : modify GoalWaypointsStamped ByteBuffer writing logics

--- src/main/kotlin/net/wavem/uvc/ros/domain/gps_navigation_msgs/GoalWaypointsStamped.kt
+++ src/main/kotlin/net/wavem/uvc/ros/domain/gps_navigation_msgs/GoalWaypointsStamped.kt
@@ -1,7 +1,9 @@
 package net.wavem.uvc.ros.domain.gps_navigation_msgs
 
 import id.jrosmessages.Message
+import net.wavem.uvc.ros.domain.geometry_msgs.Point
 import net.wavem.uvc.ros.domain.geometry_msgs.Pose
+import net.wavem.uvc.ros.domain.geometry_msgs.Quaternion
 import net.wavem.uvc.ros.domain.std_msgs.Header
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -17,7 +19,7 @@ class GoalWaypointsStamped() : Message {
 
     fun write() : ByteArray {
         val goal_waypoints_list_size : Int = this.goal_waypoints_list.size
-        val buf : ByteBuffer = ByteBuffer.allocate(this.header.size + (goal_waypoints_list_size * Double.SIZE_BYTES * 7))
+        val buf : ByteBuffer = ByteBuffer.allocate(16 + (goal_waypoints_list_size * Double.SIZE_BYTES * 7) + (goal_waypoints_list_size * 4))
         buf.order(ByteOrder.LITTLE_ENDIAN)
 
         println("GoalWaypointsStamped header : ${this.header.toString()}")
@@ -29,8 +31,15 @@ class GoalWaypointsStamped() : Message {
 
         for (pose in this.goal_waypoints_list) {
             println("GoalWaypointsStamped pose : ${pose.toString()}")
-            val poseBytes : ByteArray = pose.write()
+            val position : Point = Point(0.0, 0.0, 0.0)
+            val orientation : Quaternion = Quaternion(1.0, 2.0, 0.0, 0.0)
+
+            val goal_waypoints : Pose = Pose(position, orientation)
+            val poseBytes : ByteArray = goal_waypoints.write()
             buf.put(poseBytes)
+
+            // val poseBytes : ByteArray = pose.write()
+            // buf.put(poseBytes)
             println("GoalWaypointsStamped pose added")
         }
 
