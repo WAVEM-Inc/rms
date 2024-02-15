81bbf8e7a4f89bb91ce198b62ac097b900103eda
root <felicita64@gmail.com>
2024-02-15 00:34:40 +0000

    feat : add launch file

---
+++ launch/rcs_mqtt_bridge.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    ld: LaunchDescription = LaunchDescription()
+
+    package_name: str = 'rcs_mqtt_bridge';
+    package_shared_directory: str = get_package_share_directory(package_name);
+    executable_name: str = package_name;
+
+    rmde_executor_node: Node = Node(
+        package=package_name,
+        executable=executable_name,
+        name=executable_name,
+        output='screen',
+        parameters=[]
+    )
+
+    ld.add_action(rmde_executor_node)
+
+    return ld
+
