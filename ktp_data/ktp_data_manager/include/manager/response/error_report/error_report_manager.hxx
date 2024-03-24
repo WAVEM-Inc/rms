d50f808bf179284e33cfe42171bd6cf78b3c2a89
reidlo5135 <201840103@email.daelim.ac.kr>
2024-03-24 12:02:53 +0900

    feat : add error_reception subscription logics

--- ktp_data/ktp_data_manager/include/manager/response/error_report/error_report_manager.hxx
+++ ktp_data/ktp_data_manager/include/manager/response/error_report/error_report_manager.hxx
@@ -3,13 +3,15 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <std_msgs/msg/string.hpp>
 #include <ktp_data_msgs/msg/error_report.hpp>
 
-#define DEFAULT_QOS 10
+#include "utils/utils.hxx"
 
+#define DEFAULT_QOS 1
+
+#define ERROR_RECEPTION_TOPIC "/rms/ktp/data/notify/error/status"
 #define ERROR_REPORT_TO_ITF_TOPIC "/rms/ktp/data/error_report"
-
-#define BATTERY_STATE_ERROR_STATE_TOPIC "/sensor/battery/err/state"
 
 using std::placeholders::_1;
 using std::placeholders::_2;
@@ -22,6 +24,10 @@ namespace ktp
         {
         private:
             rclcpp::Node::SharedPtr node_;
+
+            rclcpp::CallbackGroup::SharedPtr error_reception_subscription_cb_group_;
+            rclcpp::Subscription<std_msgs::msg::String>::SharedPtr error_reception_subscription_;
+            void error_reception_subscription_cb(const std_msgs::msg::String::SharedPtr error_reception_cb);
 
             rclcpp::CallbackGroup::SharedPtr error_report_to_itf_publisher_cb_group_;
             rclcpp::Publisher<ktp_data_msgs::msg::ErrorReport>::SharedPtr error_report_to_itf_publisher_;
