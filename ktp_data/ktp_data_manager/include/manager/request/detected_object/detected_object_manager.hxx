447f641f0df152adc11ddaaf4d9e2918ae669508
reidlo5135 <201840103@email.daelim.ac.kr>
2024-03-26 16:50:20 +0900

    feat : <ktp_data_manager> add detected_object_manager logics

--- ktp_data/ktp_data_manager/include/manager/request/detected_object/detected_object_manager.hxx
+++ ktp_data/ktp_data_manager/include/manager/request/detected_object/detected_object_manager.hxx
@@ -3,7 +3,11 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <ktp_data_msgs/msg/detected_object.hpp>
+
 #define DEFAULT_QOS 10
+#define DETECTED_OBJECT_FROM_ITF_TOPIC "/rms/ktp/itf/detected_object"
+#define OBJECT_DETECT_TO_STATE_OBSTACLE_TOPIC "/drive/object_detect"
 
 using std::placeholders::_1;
 using std::placeholders::_2;
@@ -17,7 +21,13 @@ namespace ktp
         private:
             rclcpp::Node::SharedPtr node_;
 
+            rclcpp::CallbackGroup::SharedPtr detected_object_from_itf_subscription_cb_group_;
+            rclcpp::Subscription<ktp_data_msgs::msg::DetectedObject>::SharedPtr detected_object_from_itf_subscription_;
+            void detected_object_from_itf_subscription_cb(const ktp_data_msgs::msg::DetectedObject::SharedPtr detected_object_cb);
 
+            rclcpp::CallbackGroup::SharedPtr object_detect_to_state_obstacle_publisher_cb_group_;
+            rclcpp::Publisher<ktp_data_msgs::msg::DetectedObject>::SharedPtr object_detect_to_state_obstacle_publisher_;
+            void object_detect_to_state_obstacle_publish(const ktp_data_msgs::msg::DetectedObject::SharedPtr detected_object);
 
         public:
             explicit DetectedObjectManager(rclcpp::Node::SharedPtr node);
--- ktp_data/ktp_data_manager/src/manager/request/detected_object/detected_object_manager.cxx
+++ ktp_data/ktp_data_manager/src/manager/request/detected_object/detected_object_manager.cxx
@@ -3,9 +3,34 @@
 ktp::data::DetectedObjectManager::DetectedObjectManager(rclcpp::Node::SharedPtr node)
     : node_(node)
 {
+    this->detected_object_from_itf_subscription_cb_group_ = this->node_->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+    rclcpp::SubscriptionOptions detected_object_from_itf_subscription_opts;
+    detected_object_from_itf_subscription_opts.callback_group = this->detected_object_from_itf_subscription_cb_group_;
+    this->detected_object_from_itf_subscription_ = this->node_->create_subscription<ktp_data_msgs::msg::DetectedObject>(
+        DETECTED_OBJECT_FROM_ITF_TOPIC,
+        rclcpp::QoS(rclcpp::KeepLast(DEFAULT_QOS)),
+        std::bind(&ktp::data::DetectedObjectManager::detected_object_from_itf_subscription_cb, this, _1),
+        detected_object_from_itf_subscription_opts);
 
+    this->object_detect_to_state_obstacle_publisher_cb_group_ = this->node_->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+    rclcpp::PublisherOptions object_detect_to_state_obstacle_publisher_opts;
+    object_detect_to_state_obstacle_publisher_opts.callback_group = this->object_detect_to_state_obstacle_publisher_cb_group_;
+    this->object_detect_to_state_obstacle_publisher_ = this->node_->create_publisher<ktp_data_msgs::msg::DetectedObject>(
+        OBJECT_DETECT_TO_STATE_OBSTACLE_TOPIC,
+        rclcpp::QoS(rclcpp::KeepLast(DEFAULT_QOS)),
+        object_detect_to_state_obstacle_publisher_opts);
 }
 
 ktp::data::DetectedObjectManager::~DetectedObjectManager()
 {
+}
+
+void ktp::data::DetectedObjectManager::detected_object_from_itf_subscription_cb(const ktp_data_msgs::msg::DetectedObject::SharedPtr detected_object_cb)
+{
+    this->object_detect_to_state_obstacle_publish(detected_object_cb);
+}
+
+void ktp::data::DetectedObjectManager::object_detect_to_state_obstacle_publish(const ktp_data_msgs::msg::DetectedObject::SharedPtr detected_object)
+{
+    this->object_detect_to_state_obstacle_publisher_->publish(*(detected_object));
 }
