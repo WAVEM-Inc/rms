9b5cd3e56c972d4d047c566baa616b9351616fa0
reidlo5135 <201840103@email.daelim.ac.kr>
2024-03-27 18:40:25 +0900

    refactor : <ktp_data_manager> modify navigation_status_subscription_topic address

--- ktp_data/ktp_data_manager/include/manager/response/robot_status/robot_status_manager.hxx
+++ ktp_data/ktp_data_manager/include/manager/response/robot_status/robot_status_manager.hxx
@@ -24,7 +24,7 @@
 
 #define STATUS_TO_ITF_TOPIC "/rms/ktp/data/robot_status"
 #define STATUS_TO_ITF_RATE 1000
-#define ROBOT_NAVIGATION_STATUS_FROM_TASK_CTRL_TOPIC "/rms/ktp/task/robot_navigation/status"
+#define ROBOT_NAVIGATION_STATUS_FROM_TASK_CTRL_TOPIC "/rms/ktp/task/navigation/status"
 
 #define BATTERY_STATE_TOPIC "/sensor/battery/state"
 #define VELOCITY_STATE_TOPIC "/drive/velocity/state"
