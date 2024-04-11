e9536ea680c64d35b986bb2967f606f22fdfeb14
reidlo5135 <201840103@email.daelim.ac.kr>
2024-04-11 16:08:57 +0900

    refactor : <ktp_viz> modify map into naver map sdk

--- ktp_visualization/src/components/map/MapComponents.tsx
+++ ktp_visualization/src/components/map/MapComponents.tsx
@@ -1,13 +1,55 @@
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useEffect, useRef, useState } from "react";
 import "./MapComponents.css";
+import {
+    RenderAfterNavermapsLoaded,
+    NaverMap,
+    Marker,
+    Polyline,
+    Polygon,
+} from 'react-naver-maps';
 
 const MapComponent = () => {
+    const { naver } = window;
+    const mapRef: React.MutableRefObject<null> = useRef(null);
+
+    useEffect(() => {
+        if (mapRef.current && naver) {
+            const map: naver.maps.Map = new naver.maps.Map(mapRef.current, {
+                center: new naver.maps.LatLng(37.3060542, 127.2399165),
+                mapTypeId: naver.maps.MapTypeId.HYBRID,
+                zoom: 20
+            });
+
+            const marker1: naver.maps.Marker = new naver.maps.Marker({
+                position: new naver.maps.LatLng(37.3060542, 127.2399165),
+                map: map,
+                title: "TEST1"
+            });
+
+            const marker2: naver.maps.Marker = new naver.maps.Marker({
+                position: new naver.maps.LatLng(37.3061042, 127.2399165),
+                map: map,
+                title: "TEST2"
+            });
+
+            naver.maps.Event.addListener(marker1, "click", () => {
+                console.info(`marker : ${marker1.getTitle()}`);
+                console.info(`marker 1 : ${marker1.getPosition().x}`);
+            });
+
+            const polyline: naver.maps.Polyline = new naver.maps.Polyline({
+                map: map,
+                path: [
+                    marker1.getPosition(),
+                    marker2.getPosition()
+                ]
+            });
+        }
+    }, []);
+
     return (
         <div className="map_components">
-            <Map
-                className="map_container"
-                center={{ lat: 37.306595, lng: 127.240111 }}>
-            </Map>
+            <div ref={mapRef} id="map" style={{ width: "1400px", height: "800px" }} />
         </div>
     );
 };
