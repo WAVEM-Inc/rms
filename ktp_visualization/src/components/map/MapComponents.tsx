6f806fbdb41c7f8e27a016ea941145bc1c70d73f
reidlo5135 <201840103@email.daelim.ac.kr>
2024-04-14 18:31:17 +0900

    fix : <ktp_viz> error handling of mark current position

--- ktp_visualization/src/components/map/MapComponents.tsx
+++ ktp_visualization/src/components/map/MapComponents.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./MapComponents.css";
+import * as currSVG from "../../assets/img/location-dot-solid.svg";
 
 interface MapComponentProps {
     pathData: any;
@@ -9,9 +10,14 @@ interface MapComponentProps {
 const MapComponent = ({ pathData, gpsData }: MapComponentProps) => {
     const { naver } = window;
     const mapRef: React.MutableRefObject<null> = useRef(null);
-    let map: naver.maps.Map | null = null;
 
-    let gpsMarkerArray: Array<naver.maps.Marker> = [];
+    let map: naver.maps.Map | null = null;
+    const [currMarker, setCurrMarker] = useState<naver.maps.Marker | null>(null);
+    const [currentGps, setCurrentGps] = useState<any>({
+        latitude: 0.0,
+        longitude: 0.0
+    });
+
     let pathMarkerArray: Array<naver.maps.Marker> = [];
 
     const addPathMarker = (node: any): any => {
@@ -27,67 +33,75 @@ const MapComponent = ({ pathData, gpsData }: MapComponentProps) => {
         return marker;
     }
 
-    const [currentGps, setCurrentGps] = useState<any>({
-        latitude: 0.0,
-        longitude: 0.0
-    });
-
     useEffect(() => {
-        console.info(`gps : ${JSON.stringify(gpsData)}`);
-
-        if (gpsData) {
-            setCurrentGps({
-                latitude: gpsData.latitude,
-                longitude: gpsData.longitude
-            });
-        }
-    }, [gpsData]);
-
-    useEffect(() => {
-        if (mapRef.current && naver && map == null) {
+        if (mapRef.current && naver && !map) {
             const mapOpts: any = {
                 // center: new naver.maps.LatLng(37.3060542, 127.2399165),
                 center: new naver.maps.LatLng(36.1137155, 128.3676005),
                 mapTypeId: naver.maps.MapTypeId.HYBRID,
-                zoom: 19
+                zoom: 19,
+                zoomControl: true,
+                zoomControlOptions: {
+                    style: naver.maps.ZoomControlStyle.SMALL,
+                    position: naver.maps.Position.TOP_RIGHT,
+                }
             }
             map = new naver.maps.Map(mapRef.current, mapOpts);
+        } else return;
+
+        const initialCurrMarker: naver.maps.Marker = new naver.maps.Marker({
+            position: new naver.maps.LatLng(36.1137155, 128.3676005),
+            map: map,
+            title: "RobotCurrentPos",
+            // icon: process.env.PUBLIC_URL + "location-dot-solid.png"
+        });
+        setCurrMarker(initialCurrMarker);
+
+        // if (mapRef.current && naver) {
+        //     if (pathData) {
+        //         const nodeList: Array<any> = pathData.node_list;
+
+        //         for (const node of nodeList) {
+        //             console.info(`Node : ${JSON.stringify(node)}`);
+        //             const marker: naver.maps.Marker = addPathMarker(node);
+        //             pathMarkerArray.push(marker);
+        //         }
+
+        //         let path: Array<any> = [];
+        //         for (const pathMarker of pathMarkerArray) {
+        //             path.push(pathMarker.getPosition());
+        //         }
+
+        //         const polyline: naver.maps.Polyline = new naver.maps.Polyline({
+        //             map: map,
+        //             path: path
+        //         });
+        //     }
+        // }
+    }, [naver, map]);
+
+    useEffect(() => {
+        if (gpsData) {
+            // GPS 데이터 업데이트
+            setCurrentGps({
+                latitude: gpsData.latitude,
+                longitude: gpsData.longitude
+            });
+
+            currMarker!.setPosition(new naver.maps.LatLng(gpsData.latitude, gpsData.longitude));
+            currMarker!.setAnimation(naver.maps.Animation.BOUNCE);
         }
-        
-        if (mapRef.current && naver) {
-            if (currentGps) {
-                const newMarker: naver.maps.Marker = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(currentGps.latitude, currentGps.longitude),
-                    map: map,
-                    title: ""
-                });
+    }, [gpsData]);
 
-                if (newMarker.getMap()) {
-                    return;
-                }
-            }
-
-            if (pathData) {
-                const nodeList: Array<any> = pathData.node_list;
-
-                for (const node of nodeList) {
-                    console.info(`Node : ${JSON.stringify(node)}`);
-                    const marker: naver.maps.Marker = addPathMarker(node);
-                    pathMarkerArray.push(marker);
-                }
-
-                let path: Array<any> = [];
-                for (const pathMarker of pathMarkerArray) {
-                    path.push(pathMarker.getPosition());
-                }
-
-                const polyline: naver.maps.Polyline = new naver.maps.Polyline({
-                    map: map,
-                    path: path
-                });
+    useEffect(() => {
+        return () => {
+            if (map) {
+                setCurrMarker(null);
+                map.destroy();
+                map = null;
             }
         }
-    }, [naver, currentGps, pathData]);
+    }, []);
 
     return (
         <div className={"map_components"}>
